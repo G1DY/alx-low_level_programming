1124eb3ea3020440ec5a21340297c5e364210073
G1DY <kipkerring.gedion@gmail.com>
2022-09-09 12:45:45 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Programming is like building a multilingual puzzle
+ *
+ * Desription: Use the function puts
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	Return (0);
+}
+
