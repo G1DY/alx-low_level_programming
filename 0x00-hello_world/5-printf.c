97f7d918ececf508a1f7ecfabab87796c3372f2c
G1DY <kipkerring.gedion@gmail.com>
2022-09-11 09:21:15 -0700

    modified

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-
 /**
 * main - prints characters in the fuction body
-* @parameterX: none
 *
 * Description: using the main fuction
 * prints exactly with proper grammar, but the outcome is a piece of art,
-* Return: 0
+* Return: Always 0 (success)
 */
 int main(void)
+
 {
         printf("with proper grammar, but the outcome is a piece of art,\n");
+
         return (0);
 }
