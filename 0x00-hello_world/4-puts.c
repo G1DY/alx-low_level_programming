a0772a49edd8cbbad3f98e8e98504479fe6293aa
G1DY <kipkerring.gedion@gmail.com>
2022-09-11 07:37:45 -0700

    modified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
 * main - prints a string using puts
 *
 * Description: executes at the main function
-* program that prints exactly "Programming is like building a multilingual puzzle
+* program that prints exactly Programming is like building a multilingual puzzle
 * Return: 0
 */
 int main(void)
