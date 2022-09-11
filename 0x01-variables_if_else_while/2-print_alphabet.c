7defcd59e133dadfab0f89f2ea2fb1d2545cbb49
G1DY <kipkerring.gedion@gmail.com>
2022-09-11 12:24:55 -0700

    modified

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,17 +8,16 @@
  *  Return: 0
  *
  */
-
 int main(void)
 {
 	int i = 97;
 
 	while (i < 123)
+		putchar (i);
+	i++;
 	{
-		 putchar (i); i++;
+		putchar (10);
+
+		return (0);
 	}
-                 putchar (10);
-
-                 return (0);
 }
-
