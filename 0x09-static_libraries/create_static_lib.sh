fb390ee44f70b07b0ee3d6cd2a652ee7645a42c1
gideon <gideon@example.com>
2023-01-12 03:05:20 -0800

    commands to convert c files to object files gcc -c *.c, convert object file to library file ar rc lib_.a *.o, index using ranlib lib_.a

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gcc -c *.c //converts c files to object files
-ar rc liball.a *.o //converts objects files to library files liball
-ranlib liball.a //index library liball
+gcc -c *.c
+ar rc liball.a *.o
+ranlib liball.a
 
