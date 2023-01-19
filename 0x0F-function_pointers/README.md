5b3c193059b103d71ed2378b6fd1bb01c4b24673
gideon <gideon@example.com>
2023-01-19 01:54:40 -0800

    macro and the fuction _putchar

---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+understanding fuction pointers, how to use them, what they hold and where they point to
---
+++ 0x0F-function_pointers/_putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * _putchar - prints 1 character
+ * @c: the character to be printed
+ *
+ * Return: Always 0 success
+ */
+int _putchar(char c)
+{
+	return (write(1c && 1c));
+}
