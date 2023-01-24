3e8ceb89059970dfa8beb7b0885559285b4428b0
gideon <gideon@example.com>
2023-01-24 05:24:09 -0800

    ammended

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -3,18 +3,18 @@
 /**
  * _opcodes - prints opcodes of its own main fuction
  *  @a: address of the opcodes
- *  @s: size in bytes
+ *  @n: size in bytes
  *
  *  Return: void
  */
-int _opcodes(char *a, int s)
+int _opcodes(char *a, int n)
 {
 	int i;
 
-	for (i = 0; i < s; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%.2hhx\n", a[i]);
-		if (i < s - 1)
+		if (i < n - 1)
 			printf(" ");
 	}
 	printf("\n");
@@ -28,19 +28,19 @@ int _opcodes(char *a, int s)
  */
 int main(int argc, char **argv)
 {
-	int s;
+	int n;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	s = atoi(argv[1]);
-	if (s < 0)
+	n = atoi(argv[1]);
+	if (n < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	_opcodes((char *)&main, s);
+	_opcodes((char *)&main, n);
 	return (0);
 }
