fc5c358ff103f1eaea5d1d861a3a595408dbb97a
gideon <gideon@example.com>
2023-01-24 01:29:29 -0800

    A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+/**
+ * op_add - adds a and b
+ * @a: first integer
+ * @b: second interger
+ *
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtracts b from a
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - calculates the multiplication of a and b
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the multiplication of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - calculates division
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the division of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - calculates the mode of a and b
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the modulus of a and b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
