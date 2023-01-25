911fc21caa376dd8c625a3293d1de76ce362e735
gideon <gideon@example.com>
2023-01-24 22:33:28 -0800

    variadic_fuctions.h

---
+++ 0x10-variadic_functions/_putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * _putchar - writes c to std output
+ * @c: the character to be printed
+ *
+ * Return: 1 success, -1 error
+ */
+int _putchar(char c)
+{
+	return (write(1 &c 1));
+}
