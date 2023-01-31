e86b0421c0b07e0e5154ff65ee0a3a2dda365a64
gideon <gideon@example.com>
2023-01-31 00:20:42 -0800

    tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- * print_line - function before main
- * @constructor: the constructor
- *
- * Return: a string 
+ * tortoise - Apply the constructor attribute
  */
-
-void __attribute__((constructor)) print_line(void)
+void __attribute__((constructor)) tortoise(void);
+/**
+ * tortoise - implements the function
+ *
+ */
+void tortoise(void)
 {
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
