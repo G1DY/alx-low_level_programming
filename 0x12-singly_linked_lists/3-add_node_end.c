95ea654a7a71be7bbc3ba090db9f384ce4a49fff
gideon <gideon@example.com>
2023-01-30 22:48:23 -0800

    ammended

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -29,11 +29,11 @@ list_t *add_node_end(list_t **head, const char *str)
 		*head = new;
 
 	else
+	{
 		while (temp->next != NULL)
 			temp = temp->next;
-
-	temp->next = new;
-
+		temp->next = new;
+	}
 	return (*head);
 }
 
