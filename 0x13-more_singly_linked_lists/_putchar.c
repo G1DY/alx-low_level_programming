03dc5f6ef6bc879b4b80d9b6838ca70f654cdce9
gideon <gideon@example.com>
2023-02-01 01:06:01 -0800

    the header file and _putchar function

---
+++ 0x13-more_singly_linked_lists/_putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * _putchar - writes 1 character
+ * @c: the character to be printed
+ *
+ * Return: 1 success else -1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
