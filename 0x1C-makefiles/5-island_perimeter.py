66dfdcd881dc8b28cebcf77ffb5002cda8b6a1ed
Gideon <kipkerring.gedion@gmail.com>
2023-05-25 05:03:05 -0700

    a function def island_perimeter(grid): that returns the perimeter of the island described in grid

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""defines a function that returns the perimeter of 
+"""defines a function that returns the perimeter of
    the island described in grid
 """
 
@@ -25,4 +25,3 @@ def island_perimeter(grid):
                 if (i > 0 and grid[i - 1][j] == 1):
                     edges += 1
     return size * 4 - edges * 2
-
